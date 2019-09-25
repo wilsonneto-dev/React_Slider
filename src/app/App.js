7161c0c8a1bc6c00cbf77251062db19ab2c2473f
Wilson Neto <wilson.neto@encripta.com.br>
2019-09-25 11:05:01 -0300

    work in progress: propriedades do slider

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,20 @@ import Slider from '../components/Slider';
 
 import '../assets/styles/global.scss'
 
-
+const items = [
+  { title: '1' },
+  { title: '2'},
+  { title: '3'},
+  { title: '4'},
+  { title: '5'},
+  { title: '6'},
+  { title: '7'}
+];
 
 function App() {
   return (
     <div className="App">
-      <Slider />
+      <Slider title="testando" number="5" items={items} />
     </div>
   );
 }
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './index.scss'; 
 
@@ -9,6 +10,24 @@ export default class Slider extends Component {
     this.state = { position: 0 };
 
     this.handlePosition = this.handlePosition.bind(this);
+    this.processStyle = this.processStyle.bind(this);
+  }
+
+  static defaultProps = {
+    number: 3,
+    itemWidth: 100,
+    moveItems: 1
+  }
+
+  static propTypes = {
+    items: PropTypes.array.isRequired,
+    title: PropTypes.string
+  }
+
+  processStyle(){
+    const { position } = this.state;
+    const pixels = position * 100;
+    return {transform: `translate(${pixels}px)`}
   }
 
   handlePosition(moves) {
@@ -16,35 +35,28 @@ export default class Slider extends Component {
   }
 
   render(){
-    const { position } = this.state;
-    const pixels = position * 100;
+    const { items } = this.props; 
 
     return (
       <>
-        <div className="slider">
-          <ul style={{transform: `translate(${pixels}px)`}}>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>10</li>
-          </ul>
-        </div>
-
-        <div className="controls">
-          <button onClick={f => { this.handlePosition(-2) } }> prev </button>
-          <button onClick={f => { this.handlePosition(2) } }> next </button>
-          <br />
-          Position: { position } / Pixels: { pixels }
-        </div>
-
+          <div className="slider">
+            <ul style={this.processStyle()}>
+              { items.map(( item, i) => {
+                return (
+                  <li>
+                    <div>
+                      {i}
+                    </div>
+                  </li>
+                );
+              }) }
+            </ul>
+            <div className="controls">
+              <button onClick={f => { this.handlePosition(-2) } }> &lt;&lt; </button>
+              <button onClick={f => { this.handlePosition(2) } }> &gt;&gt; </button>
+            </div>
+          </div>
       </>
     );
   }
-
 }
